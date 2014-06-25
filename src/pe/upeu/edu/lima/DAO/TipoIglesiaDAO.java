356d6676f8403387ec7cd61f8b7c98131d669ea2
juan <juan@192.168.20.175>
2014-06-25 16:02:57 -0500

    version3:Desarollo de clases2

--- src/pe/upeu/edu/lima/DAO/TipoIglesiaDAO.java
+++ src/pe/upeu/edu/lima/DAO/TipoIglesiaDAO.java
@@ -6,12 +6,46 @@
 
 package pe.upeu.edu.lima.DAO;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import pe.upeu.edu.lima.config.Conexion;
+import pe.upeu.edu.lima.modelo.Iglesia;
+import pe.upeu.edu.lima.modelo.TipoIglesia;
+
 /**
  *
  * @author juan
  */
 public class TipoIglesiaDAO {
+     private Connection cx;
+   private Statement st;
+    private ResultSet rs;
+    private String sql;
     
+    ArrayList<TipoIglesia> lista = null;
+    
+    public ArrayList<TipoIglesia> listarTipoIglesia(){
+        try {
+            lista = new ArrayList();
+            sql = "SELECT * FROM TipoIglesia";
+            cx = Conexion.getConexion();
+            st = cx.createStatement();
+            rs = st.executeQuery(sql);
+            while(rs.next()){
+                Iglesia d= new Iglesia();
+                d.setIdi(rs.getInt("idiglesia"));
+                d.setIdd(rs.getInt("iddistrito"));
+                d.setIdd(rs.getInt("distrito"));
+                d.setIdd(rs.getInt("estado"));
+                
+            }
+        } catch (Exception e) {
+           
+        }
+        return lista;
+    }
     
     
 }
